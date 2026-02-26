670a1dc5d6c27174b639495ffaaf2ccb271ebaf6
9yCrooked <3444364899@qq.com>
2026-02-26 20:48:35 +0800

    feat(导航项图标): 新增NavItemIconHelper扩展功能
    添加导航项图标帮助类，支持以下功能：
    1. 双态图标系统：支持选中和未选中状态显示不同图标
    2. 通知圆点显示控制
    3. 静态图标可见性控制
    4. 与WinUI 3 NavigationView集成
    
    同时添加ObjectConverter工具类，提供WinRT与COM对象间的转换功能

---
+++ NexusCore/Helpers/NavItemIconHelper.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "pch.h"
+#include "Helpers/NavItemIconHelper.g.h"
+
+namespace winrt::NexusCore::Helpers::implementation
+{
+    struct NavItemIconHelper : NavItemIconHelperT<NavItemIconHelper>
+    {
+        NavItemIconHelper() = default;
+
+        static winrt::Microsoft::UI::Xaml::DependencyProperty SelectedIconProperty();
+        static winrt::Microsoft::UI::Xaml::DependencyProperty ShowNotificationDotProperty();
+        static winrt::Microsoft::UI::Xaml::DependencyProperty UnselectedIconProperty();
+        static winrt::Microsoft::UI::Xaml::DependencyProperty StaticIconVisibilityProperty();
+
+        // 获取和设置附加属性的方法
+        static winrt::Windows::Foundation::IInspectable GetSelectedIcon(winrt::Microsoft::UI::Xaml::DependencyObject const& obj);
+        static void SetSelectedIcon(winrt::Microsoft::UI::Xaml::DependencyObject const& obj, winrt::Windows::Foundation::IInspectable const& value);
+        static bool GetShowNotificationDot(winrt::Microsoft::UI::Xaml::DependencyObject const& obj);
+        static void SetShowNotificationDot(winrt::Microsoft::UI::Xaml::DependencyObject const& obj, bool value);
+        static winrt::Windows::Foundation::IInspectable GetUnselectedIcon(winrt::Microsoft::UI::Xaml::DependencyObject const& obj);
+        static void SetUnselectedIcon(winrt::Microsoft::UI::Xaml::DependencyObject const& obj, winrt::Windows::Foundation::IInspectable const& value);
+        static winrt::Microsoft::UI::Xaml::Visibility GetStaticIconVisibility(winrt::Microsoft::UI::Xaml::DependencyObject const& obj);
+        static void SetStaticIconVisibility(winrt::Microsoft::UI::Xaml::DependencyObject const& obj, winrt::Microsoft::UI::Xaml::Visibility const& value);
+
+    private:
+		static winrt::Microsoft::UI::Xaml::DependencyProperty s_selectedIconProperty;
+		static winrt::Microsoft::UI::Xaml::DependencyProperty s_showNotificationDotProperty;
+		static winrt::Microsoft::UI::Xaml::DependencyProperty s_unselectedIconProperty;
+		static winrt::Microsoft::UI::Xaml::DependencyProperty s_staticIconVisibilityProperty;
+    };
+}
+
+namespace winrt::NexusCore::Helpers::factory_implementation
+{
+    struct NavItemIconHelper : NavItemIconHelperT<NavItemIconHelper, implementation::NavItemIconHelper> {};
+}
